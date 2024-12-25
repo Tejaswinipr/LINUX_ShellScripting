9d86dab8a7629bd1d0a2b591d164083b1684e110
Tejaswini P R <67628985+Tejaswinipr@users.noreply.github.com>
2024-12-25 17:48:32 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##########################
+# About: Print the list of users who has read access in the organisation 
+# Github API Integration
+# Input: Export Username, Token and Organisation, Repo name
+# 
+# Owner: Tejaswini
+##########################
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -36,6 +45,13 @@ function list_users_with_read_access {
     fi
 }
 
+function helper {
+expected_cmd_args=2
+if [ $# -ne $expected_cmd_args]; 
+then
+    echo "Please execute the script with required cmd args"
+    echo "asd"
+}
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
